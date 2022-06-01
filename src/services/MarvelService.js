654cefc252ba83d72092e44a4fa72e3acebd8d8a
akoval29 <akoval29@gmail.com>
2022-06-02 02:50:08 +0300

    fetchTest1

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,9 @@
 
 
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=8711c7ec1be344e934d90cf8b671fb6c';
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -12,12 +15,20 @@ class MarvelService {
     }
 
     getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=8711c7ec1be344e934d90cf8b671fb6c');
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+    
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`https://gateway.marvel.com:443/v1/public/characters/${id}?&apikey=8711c7ec1be344e934d90cf8b671fb6c`);
-    }
+    // getAllCharacters = () => {
+    //     return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=8711c7ec1be344e934d90cf8b671fb6c');
+    // }
+
+    // getCharacter = (id) => {
+    //     return this.getResource(`https://gateway.marvel.com:443/v1/public/characters/${id}?&apikey=8711c7ec1be344e934d90cf8b671fb6c`);
+    // }
 }
 
 export default MarvelService;
