a9d759d4d328db8db1cca104a406c98e2a498444
akoval29 <akoval29@gmail.com>
2022-06-04 03:18:46 +0300

    get_random_char_1

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -27,13 +27,7 @@ class RandomChar extends Component {
         this.marvelService
             .getCharacter(id)
             .then(res => {
-                this.setState({
-                    name: res.data.results[0].name,
-                    description: res.data.results[0].description,
-                    thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extention,
-                    homepage: res.data.results[0].urls[0].url,
-                    wiki: res.data.results[0].urls[1].url,
-                })
+                this.setState(res)
             })
     }
 
