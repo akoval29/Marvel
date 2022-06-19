24081b69e5f05c74e42b944ace888c48b9c03110
akoval29 <akoval29@gmail.com>
2022-06-19 13:15:55 +0300

    Charnfo almost

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -11,6 +11,8 @@ class CharInfo extends Component {
         char: null,
         loading: false,
         error: false,
+        // showMore: false,
+        // buttonShowMore: true,
     }
 
     marvelService = new MarvelService();
@@ -25,7 +27,7 @@ class CharInfo extends Component {
         this.updateChar();
     }
 
-    updateChar= () => {
+    updateChar = () => {
         const {charId} = this.props;
         if(!charId) {
             return;
@@ -59,29 +61,47 @@ class CharInfo extends Component {
         })
     }
 
+    // onButtonShowMore = () => {
+    //     this.setState((state) => {
+    //         return {
+    //             showMore: !state.showMore,
+    //             buttonShowMore: !state.buttonShowMore,
+    //         }
+    //     })
+    // }
+
+    // onShowMore = () => {
+    //     this.state.showMore ? <View/> : <View/>
+    // }
 
     render() {
-        const{char, loading, error} = this.state;
+        const {char, loading, error} = this.state;
         const skeleton = char || loading || error ? null : <Skeleton/>
         const errorMSG = error ? <ErrorMSG/> :null;
         const spinner = loading ? <Spinner/> :null;
-        const content = !(loading || error || !char) ? <View char={char}/> :null;
+        const content = !(loading || error || !char) ? <View char={char}/> : null;
         return (
             <div className="char__info">
                 {skeleton}
                 {errorMSG}
                 {spinner}
                 {content}
+                {/* <button onClick={this.onShowMore}>{this.state.buttonShowMore ? '< Show more >': '< Show less >'}</button> */}
             </div>
         )
     }
+
 }
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
     return(
         <>
         <div className="char__basics">
-            <img src={thumbnail} alt={name}/>
+            <img src={thumbnail} alt={name} style={imgStyle}/>
             <div>
                 <div className="char__info-name">{name}</div>
                 <div className="char__btns">
@@ -99,8 +119,11 @@ const View = ({char}) => {
         </div>
         <div className="char__comics">Comics:</div>
         <ul className="char__comics-list">
+            {comics.length > 0 ? null : "No comics yet"}
             {
                 comics.map((item, i) => {
+                    // {this.state.showMore ? null : if (i > 9) return}
+                    if (i > 9) return
                     return (
                         <li key={i} className="char__comics-item">
                         {item.name}
@@ -108,9 +131,6 @@ const View = ({char}) => {
                     )
                 })
             }
-            <li className="char__comics-item">
-                All-Winners Squad: Band of Heroes (2011) #3
-            </li>
         </ul>
         </>
     )
