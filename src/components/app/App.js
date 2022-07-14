e9f32b5d90d40b9e237c9ed4bce020e072cdb8db
akoval29 <akoval29@gmail.com>
2022-07-14 17:22:09 +0300

    CharList.js_hoocked

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,37 +8,36 @@ import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-    const [selectedChar, setChar] = useState(null);
+  const [selectedChar, setChar] = useState(null);
 
-    let [showRandomChar, setRandomChar]= useState(true);
+  let [showRandomChar, setRandomChar]= useState(true);
 
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
+  const onCharSelected = (id) => {
+    setChar(id);
+  }
 
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {showRandomChar ? <RandomChar/> : null}
-                
-                <div className="randomchar__btns" onClick={() => setRandomChar(showRandomChar = !showRandomChar)}>
-                <a href={null} className="button button__main">
-                    <div className="inner">{showRandomChar ? 'Hide this': 'Show this'}</div>
-                </a>
-                </div> 
-                
-                <div className="char__content">
-                    <CharList onCharSelected={onCharSelected}/>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
+  return (
+    <div className="app">
+      <AppHeader/>
+      <main>
+        {showRandomChar ? <RandomChar/> : null}
+        
+        <div className="randomchar__btns" onClick={() => setRandomChar (showRandomChar = !showRandomChar)}>
+        <a href={null} className="button button__main">
+          <div className="inner">{showRandomChar ? 'Hide this': 'Show this'}</div>
+        </a>
         </div>
-    )
-    
+        
+        <div className="char__content">
+          <CharList onCharSelected={onCharSelected}/>
+          <ErrorBoundary>
+            <CharInfo charId={selectedChar}/>
+          </ErrorBoundary>
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision"/>
+      </main>
+    </div>
+  )
 }
 
 export default App;
